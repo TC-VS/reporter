c22f8d90ba61ea6ed0d0fdcb1008d35f9dbfdab6
TC-VS <56305881+TC-VS@users.noreply.github.com>
2019-10-17 12:26:49 +0200

    adds_header_creation

--- tests/test_reporter.py
+++ tests/test_reporter.py
@@ -1,2 +1,15 @@
-def test_reporter():
-    assert True
+from reporter.header import create_header
+
+def test_create_header():
+    auth_list =[
+        {"first" : "Jean", "last" : "Dupont"},
+        {"first" : "Eric", "last" : "Martin"},
+    ]
+    
+    res = create_header(auth_list,"NY")
+    if not "Jean" in res:
+        raise ValueError ('Prénom manquant')
+        
+    assert "NY" in res
+        
+test_create_header()
