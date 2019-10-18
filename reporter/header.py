a6baae33576d27c0ec74add66656822e8f03e4ed
TC-VS <56305881+TC-VS@users.noreply.github.com>
2019-10-18 14:55:18 +0200

    Update header.py

--- reporter/header.py
+++ reporter/header.py
@@ -1,5 +1,7 @@
-
+import json
 import datetime
+
+
 def create_header(authors_list, loc="Paris"):
     """Creates the header text.
     
@@ -23,11 +25,40 @@ def create_header(authors_list, loc="Paris"):
     
     
     for author in authors_list:
-        first = author.get("first", "")
-        last = author.get("last", "")
+        first = author.get("firstname", "")
+        last = author.get("lastname", "")
         if not last:
             print('No value for last')
         author_string = f'- {first} {last}'
 
         author_strings.append(author_string)
     return "\n".join(author_strings)
+
+
+
+
+
+
+
+    # Fonction qui ouvre le fichier JSON, et afficher create_header avec le contenu du JSON
+# appel fonction create_header
+# retourne le contenu du JSON
+def header_from_json(json_path):
+    # appel du fichier
+
+    """Creates the header depuis une fichier json.
+       Ouvre un fichier json
+
+
+    """
+
+    with open(json_path, "r") as fh:
+        authors = json.load(fh)
+    
+    result = create_header(authors)
+        
+    return result
+
+#ci-dessous code pour tester la fonction
+path = r"C:\Users\thierry.clavier\Documents\reporter\data\authors.json" # on met r devant le chemin pour indiquer qu'il s'agit d'un chemin windows complet         
+header_from_json(path)
